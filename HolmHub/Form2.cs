a3fef4ff5a9a4e46adb520db12a25ad48885a624
Holmeren <ludvig.andreas@hotmail.com>
2023-01-01 20:03:49 +0100

    restored setting in new machien enviroment

--- HolmHub/Form2.cs
+++ HolmHub/Form2.cs
@@ -14,47 +14,45 @@ namespace HolmHub
     public partial class Form2 : Form
     {
 
-        public delegate void LicenseHandler(Form sender, LicenseEventArgs e);
+        //public delegate void LicenseHandler(Form sender, LicenseEventArgs e);
 
+        ////Checks if HolmHub folder in LocalAppData exists, if not it creates it.
+        //private void EventHandler1(Form sender, LicenseEventArgs e)
+        //{
+        //    // Do something
+        //}
 
+        ////Checks if ClientLicense exists, if not pormpt user to insert it.
+        //private void Event2(Form sender, EventArgs e)
+        //{
+        //    // Do something
+        //}
 
-        //Checks if HolmHub folder in LocalAppData exists, if not it creates it.
-        private void EventHandler1(Form sender, LicenseEventArgs e)
-        {
-            // Do something
-        }
+        ////Extracts License from ClientLicense.
+        //private void Event3(Form sender, EventArgs e)
+        //{
+        //    string directory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "HolmHub");
+        //    if (!Directory.Exists(directory))
+        //    {
+        //        Directory.CreateDirectory(directory);
+        //    }
 
-        //Checks if ClientLicense exists, if not pormpt user to insert it.
-        private void Event2(Form sender, EventArgs e)
-        {
-            // Do something
-        }
+        //    string filepath = Path.Combine(directory, "ClientLicense.dat");
+        //    string base64String = File.ReadAllText(filepath);
 
-        //Extracts License from ClientLicense.
-        private void Event3(Form sender, EventArgs e)
-        {
-            string directory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "HolmHub");
-            if (!Directory.Exists(directory))
-            {
-                Directory.CreateDirectory(directory);
-            }
-
-            string filepath = Path.Combine(directory, "ClientLicense.dat");
-            string base64String = File.ReadAllText(filepath);
-
-            string SteppingStone = Encoding.UTF8.GetString(Convert.FromBase64String(base64String));
-            string decodedString = Encoding.UTF8.GetString(Convert.FromBase64String(SteppingStone));
-            Debug.WriteLine(decodedString);
-        }
+        //    string SteppingStone = Encoding.UTF8.GetString(Convert.FromBase64String(base64String));
+        //    string decodedString = Encoding.UTF8.GetString(Convert.FromBase64String(SteppingStone));
+        //    Debug.WriteLine(decodedString);
+        //}
 
 
         public Form2()
         {
             InitializeComponent();
-            this.Load += new LicenseHandler(EventHandler1);
-            this.Load += new LicenseHandler(Event1);
-            this.Load += new LicenseHandler(Event2);
-            this.Load += new LicenseHandler(Event3);
+            //this.Load += new LicenseHandler(EventHandler1);
+            //this.Load += new LicenseHandler(Event1);
+            //this.Load += new LicenseHandler(Event2);
+            //this.Load += new LicenseHandler(Event3);
         }
 
         private void Form2_Load(object sender, EventArgs e)
